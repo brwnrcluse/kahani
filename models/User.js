b412abbc29e8a98437dfc6e789f1ce5a461caf25
Anoopdeep Bal <anoopdeep@gmail.com>
2019-02-11 16:15:48 +0100

    auth infrastructure done except for rendering user views

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const userSchema = new User(
+const userSchema = new Schema(
   {
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
